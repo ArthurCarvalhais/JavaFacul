fbdab4dbeb349a04dc05c13e644ecdbd392bdcb1
Arthur Carvalhais <arthurcarvalhais11@gmail.com>
2025-03-12 21:12:34 -0300

    exe

---
+++ Lista1Java/ContaCliente/src/exe3/Produto.java
@@ -0,0 +1,11 @@
+public class Produto {
+    public int id, qtde;
+    public String descricao;
+    public float preco;
+
+    // criar um produto sem valores
+    public Produto(){
+        this.descricao = "sem descrição";
+        
+    }
+}
